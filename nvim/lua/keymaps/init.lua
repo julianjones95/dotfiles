e36255033442c3b9dcdab780f8a9346926075294
Julian Jones <61437346+julianjones95@users.noreply.github.com>
2024-04-20 15:47:32 -0400

    Enable suspension shortcut
    Update nvim/lua/keymaps/init.lua to enable suspension shortcut using the characters fg

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -33,6 +33,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagn
 -- Shortcut to create a new tab
 vim.keymap.set('n','vv', ':tabnew . ')
 
+-- Shortcut to suspend the current instance of nvim
+vim.keymap.set('n','fg', ':suspend ')
+
 -- Shortcut to Telescope find files 
 vim.keymap.set('n','vvv', ':Telescope find_files ')
 
